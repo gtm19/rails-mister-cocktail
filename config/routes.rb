fdce5832e583c93bb92bc29327d00f45e7a1d768
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-01-30 15:39:16 +0000

    created cocktails controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
+  get 'cocktails/create'
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
