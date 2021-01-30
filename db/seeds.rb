8e36349c513fa769abe5048eec2950472343f7d8
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-01-30 13:51:06 +0000

    added ingredient seeds to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+p "Deleting all ingredients"
+Ingredient.destroy_all
+
+ingredient_url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = JSON.parse(URI.open(ingredient_url).read)
+
+ingredients.flatten[1].each do |array|
+  ingredient = array.values.first
+  p "Adding #{ingredient} to db"
+  Ingredient.create(name: ingredient)
+end
