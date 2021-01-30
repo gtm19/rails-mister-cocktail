5a1c902add15cb927cafe5d84961ee5ff85455d0
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-01-30 12:24:12 +0000

    completed models, associations, and validations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,7 @@
 class Ingredient < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  has_many :doses
+  # Note that line below is causing rake to fail, but it gives a nicer
+  # error message so I don't care 🤷‍♂️
+  has_many :cocktails, through: :doses, dependent: :restrict_with_error
 end
