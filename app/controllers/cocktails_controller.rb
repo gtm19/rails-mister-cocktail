be3aef9fe5cc487fbfef88fe861163e0210d447e
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-02-02 21:25:53 +0000

    added new dose for to show view. Need to connect it all up still

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -7,7 +7,8 @@ class CocktailsController < ApplicationController
   end
 
   def show
-
+    @dose = Dose.new
+    @ingredients = Ingredient.all
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get "cocktails/random", to: "cocktails#random"
-  resources :cocktails, only: [:index, :show, :new, :create]
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
