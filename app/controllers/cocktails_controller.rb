c48bed6e3569746fab287492d36305c84e6e42ea
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-02-02 20:48:36 +0000

    complete new view and methods

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -11,9 +11,17 @@ class CocktailsController < ApplicationController
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render "new"
+    end
   end
 
   def random
@@ -25,5 +33,9 @@ class CocktailsController < ApplicationController
   
   def set_cocktail
     @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
